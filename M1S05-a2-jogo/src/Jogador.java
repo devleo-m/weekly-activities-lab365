e86528421ba1dec7ec37210b3deb3c49502f4e4f
dev-leomadeira <149954966+devleo-m@users.noreply.github.com>
2024-02-06 18:10:01 -0300

    CREATE GAME

---
+++ M1S05-a2-jogo/src/Jogador.java
@@ -0,0 +1,17 @@
+public class Jogador {
+    String nome;
+    int idade;
+    int pontuacao = 0;
+    int numeroTentivas;
+
+    public void adicionaPontos(){
+
+    }
+    public void perdePontos(){
+
+    }
+    public void adicionaTentativa(){
+
+    }
+
+}
---
+++ M1S05-a2-jogo/src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Hello world!");
+    }
+}
